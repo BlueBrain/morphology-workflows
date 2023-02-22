f7ead1250e0588ee66256d0367b2b6dae7eba00f
Adrien Berchet <adrien.berchet@epfl.ch>
2023-02-22 22:38:03 +0100

    Remove Py3.11 because of AllenSDK

--- setup.py
+++ setup.py
@@ -84,7 +84,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
 )
